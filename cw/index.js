8de2827ca5de432f2e4c16aefffd596d7340c063
sharKiritharan <98522486+sharKiritharan@users.noreply.github.com>
2024-03-26 17:36:36 +0000

    added s3 requirements (kinda))

---
+++ cw/index.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const routes = require("./src/routes/app");
+const session = require("express-session");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// set base path for relative pug template extends
+app.locals.basedir = path.join(__dirname, "src/views");
+
+app.set("view engine", "pug");
+app.set("views", "./src/views");
+
+app.use(session({ secret: "s" }));
+app.use(express.static("./src/public"));
+app.use("/", routes);
+app.use((req, res, next) => {
+    res.status(404).render("404");
+});
+app.listen(port, () => {
+    console.log("Server running on http://localhost:" + port);
+});
+
+
